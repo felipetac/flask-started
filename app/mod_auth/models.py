637b7760e584af1738278748cf2376895377e5c7
Felipe Toscano <felipe.toscano@gmail.com>
2018-07-25 19:28:22 -0300

    Correção identação

--- app/mod_auth/models.py
+++ app/mod_auth/models.py
@@ -18,12 +18,11 @@ class User(Base):
     __tablename__ = 'auth_user'
 
     # User Name
-    name = db.Column(db.String(128),  nullable=False)
+    name = db.Column(db.String(128), nullable=False)
 
     # Identification Data: email & password
-    email = db.Column(db.String(128),  nullable=False,
-                         unique=True)
-    password = db.Column(db.String(192),  nullable=False)
+    email = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(192), nullable=False)
 
     # Authorisation Data: role & status
     #role = db.Column(db.SmallInteger, nullable=False, default=1)
@@ -34,7 +33,6 @@ class User(Base):
 
     # New instance instantiation procedure
     def __init__(self, name, email, password):
-
         self.name = name
         self.email = email
         self.password = password
