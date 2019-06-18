c9e077f262300e473c0559ce8fefedc84398b7c0
Felipe Cardoso <felipe.cardoso@serpro.gov.br>
2019-06-18 18:27:44 -0300

    Implementação parcial das controladoras

--- app/mod_auth/controller/group.py
+++ app/mod_auth/controller/group.py
@@ -12,4 +12,48 @@ def list_groups():
     if groups:
         group_schema = GroupSchema(many=True)
         return jsonify(group_schema.dump(groups).data)
-    return jsonify("Não há usuários cadastrados na base!"), 204
+    return jsonify("Não há grupos cadastrados na base!"), 204
+
+
+@MOD_AUTH.route('/group', methods=['POST'])
+def create_group():
+    form = GroupForm.from_json(request.get_json())
+    if form.validate_on_submit():
+        group = Group()
+        form.populate_obj(group)
+        DB.session.add(group)
+        DB.session.commit()
+        return jsonify("Grupo criado com sucesso!")
+    return jsonify(form.errors), 406
+
+
+@MOD_AUTH.route('/group/<int:group_id>', methods=['GET'])
+def read_group(group_id):
+    group = Group.query.filter_by(id=group_id).first()
+    if group:
+        group_schema = GroupSchema()
+        return jsonify(group_schema.dump(group))
+    return jsonify("Não há grupos cadastrados na base!"), 204
+
+
+@MOD_AUTH.route('/group/<int:group_id>', methods=['PUT'])
+def update_group(group_id):
+    group = Group.query.filter_by(id=group_id).first()
+    if group:
+        form = GroupForm.from_json(request.get_json(), obj=group) # set the object to avoid raising a ValidationError  
+        if form.validate_on_submit():
+            form.populate_obj(group)
+            DB.session.commit()
+            return jsonify("Grupo atualizado com sucesso!")
+        return jsonify(form.errors), 406
+    return jsonify("Id de usuário não encontrado!"), 204
+
+
+@MOD_AUTH.route('/group/<int:group_id>', methods=['DELETE'])
+def delete_group(group_id):
+    group = Group.query.filter_by(id=group_id).first()
+    if group:
+        DB.session.delete(group)
+        DB.session.commit()
+        return jsonify("Grupo apagado com sucesso!")
+    return jsonify("Id de usuário não encontrado ou já deletado!"), 204
--- app/mod_auth/controller/role.py
+++ app/mod_auth/controller/role.py
@@ -11,5 +11,49 @@ def list_roles():
     roles = Role.query.all()
     if roles:
         role_schema = RoleSchema(many=True)
-        return jsonify(role_schema.dump(roles).data)
-    return jsonify("Não há usuários cadastrados na base!"), 204
+        return jsonify(role_schema.dump(roles))
+    return jsonify("Não há regras cadastrados na base!"), 204
+
+
+@MOD_AUTH.route('/role', methods=['POST'])
+def create_role():
+    form = RoleForm.from_json(request.get_json())
+    if form.validate_on_submit():
+        role = Role()
+        form.populate_obj(role)
+        DB.session.add(role)
+        DB.session.commit()
+        return jsonify("Regra criada com sucesso!")
+    return jsonify(form.errors), 406
+
+
+@MOD_AUTH.route('/role/<int:role_id>', methods=['GET'])
+def read_role(role_id):
+    role = Role.query.filter_by(id=role_id).first()
+    if role:
+        role_schema = RoleSchema()
+        return jsonify(role_schema.dump(role))
+    return jsonify("Não há regras cadastradas na base!"), 204
+
+
+@MOD_AUTH.route('/role/<int:role_id>', methods=['PUT'])
+def update_role(role_id):
+    role = Role.query.filter_by(id=role_id).first()
+    if role:
+        form = RoleForm.from_json(request.get_json(), obj=role) # set the object to avoid raising a ValidationError  
+        if form.validate_on_submit():
+            form.populate_obj(role)
+            DB.session.commit()
+            return jsonify("Regra atualizada com sucesso!")
+        return jsonify(form.errors), 406
+    return jsonify("Id de regra não encontrado!"), 204
+
+
+@MOD_AUTH.route('/role/<int:role_id>', methods=['DELETE'])
+def delete_role(role_id):
+    role = Role.query.filter_by(id=role_id).first()
+    if role:
+        DB.session.delete(role)
+        DB.session.commit()
+        return jsonify("Regra apagada com sucesso!")
+    return jsonify("Id de regra não encontrado ou já deletado!"), 204
